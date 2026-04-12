1a62577e9e67a9be3ee1ec63b8139db4c9be9244
ramom06 <rmartinespana@gmail.com>
2026-04-12 16:39:31 +0200

    inscripcion ok

--- ProyectoCampeonatoTFG/src/app/app.routes.ts
+++ ProyectoCampeonatoTFG/src/app/app.routes.ts
@@ -4,12 +4,10 @@ import { CampeonatoListComponent }    from './campeonato-list/campeonato-list';
 import { CampeonatoAdminComponent }   from './campeonato-admin/campeonato-admin';
 import { AdminLoginComponent }        from './admin-login/admin-login';
 import { guards }                 from './guards/guards';
-import {SorteoComponent} from './sorteo/sorteo';
 
 export const routes: Routes = [
   { path: '', component: CampeonatoListComponent, title: 'Campeonatos' },
   { path: 'campeonato/:id', component: CampeonatoDetalle, title: 'Detalle Campeonato' },
   { path: 'admin/login', component: AdminLoginComponent, title: 'Acceso Admin' },
   { path: 'admin/campeonatos', component: CampeonatoAdminComponent, title: 'Gestión de Campeonatos', canActivate: [guards] },
-  { path: 'campeonato/:idCampeonato/categoria/:idCategoria/sorteo', component: SorteoComponent, title: 'Sorteo' },
 ];
