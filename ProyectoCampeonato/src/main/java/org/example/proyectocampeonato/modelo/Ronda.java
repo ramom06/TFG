e5ebd38b2b176f9b7e64536d4753a5586769a9c0
ramom06 <rmartinespana@gmail.com>
2026-04-16 18:26:18 +0200

    Sorteo

---
+++ ProyectoCampeonato/src/main/java/org/example/proyectocampeonato/modelo/Ronda.java
@@ -0,0 +1,17 @@
+package org.example.proyectocampeonato.modelo;
+
+import java.util.List;
+
+public class Ronda {
+    //Final , semifinal..
+    private String nombre;
+
+    //1/8, 1/4...
+    private String ronda;
+
+    //??
+    private int orden;
+
+    private List<Combate> combates;
+
+}
---
+++ ProyectoCampeonato/src/main/java/org/example/proyectocampeonato/modelo/Sorteo.java
@@ -0,0 +1,13 @@
+package org.example.proyectocampeonato.modelo;
+
+public class Sorteo {
+    private Long idCampeonato;
+    private Long idCategoria;
+    private String nombreCategoria;
+    private String nombreCampeonato;
+    private String modalidad;
+    private String genero;
+    private List<RondaDTO> rondas;
+}
+
+
